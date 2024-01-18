2313fc845c6262387ea13a66dbf182c269308284
Rodrigo Brezolin <rodrigobrezolin@MacBook-Air-de-Rodrigo.local>
2024-01-18 19:40:05 -0300

    estudos vue - inicio

---
+++ vue/vue-project/src/App.vue
@@ -0,0 +1,42 @@
+<script>
+import { ref } from 'vue';
+
+const list = ref(
+  [
+    {
+      name: "Rodrigo",
+    },
+    {
+      name: "Louize"
+    }
+  ]
+) 
+
+export default {
+  data() {
+    return {
+      list: list,
+    };
+  },
+  methods: {
+   
+  }
+};
+</script>
+
+<template>
+  <main>
+    <p v-for="item in list">
+     {{ item.name }}
+    </p>
+
+  </main> 
+</template>
+
+
+<style scoped>
+p {
+  color: white;
+  font-size: 32px;
+}
+</style>
