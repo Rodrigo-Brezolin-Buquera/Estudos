3ef64258d763f3bced3a49ddfbb269aa0af423c5
Rodrigo Brezolin <rodrigobrezolin@MacBook-Air-de-Rodrigo.local>
2024-01-26 17:48:37 -0300

    props

--- vue/vue-project/src/App.vue
+++ vue/vue-project/src/App.vue
@@ -1,38 +1,66 @@
 <script>
-import { ref } from 'vue';
+import { ref } from "vue";
+import axios from "axios";
+import Card from "./components/Card.vue"
 
-const list = ref(
-  [
-    {
-      name: "Rodrigo",
-    },
-    {
-      name: "Louize"
-    }
-  ]
-) 
+// dados fixo precisam do ref
+// const list = ref(
+//   [
+//     {
+//       name: "Rodrigo",
+//     },
+//     {
+//       name: "Louize"
+//     }
+//   ]
+// )
 
 export default {
   data() {
     return {
-      list: list,
+      list: null,
+      loading: false
     };
   },
   methods: {
-   
-  }
+    async getList() {
+      try {
+        this.loading = true
+        const res = await axios.get(
+          "https://rickandmortyapi.com/api/character"
+        );
+        this.list = res.data.results;
+        this.loading = false
+
+        console.log(this.list);
+      } catch (error) {
+        this.loading = false
+
+        console.log(error);
+      }
+    },
+  },
+  // mounted() {
+  //   this.getList();
+  // },
+  components: {
+    Card,
+  },
 };
 </script>
 
 <template>
   <main>
-    <p v-for="item in list">
-     {{ item.name }}
-    </p>
 
-  </main> 
+
+    <button :disabled="loading" @click="getList">Get list</button>
+    <p v-if="loading" role="alert">Loading your list</p>
+    <div v-else>
+      <Card v-for="item in list" :key="item.id" :name="item.name" /> 
+              
+    </div>
+  </main>
 </template>
-
 
 <style scoped>
 p {
---
+++ vue/vue-project/src/components/Card.vue
@@ -0,0 +1,25 @@
+<script>
+
+export default {
+  props: {
+    name: String,
+  }
+};
+
+</script>
+
+<template>
+  <section>
+    <p>{{ name }}</p>
+  </section>
+</template>
+
+<style scoped>
+section {
+  background-color: grey;
+  width: 500px;
+  height: 100px;
+  color: white;
+  font-size: 32px;
+}
+</style>
