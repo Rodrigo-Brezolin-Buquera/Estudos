bf96d6cca413f1238735071c747e9b2732bec27f
Rodrigo-Brezolin-Buquera <rodrigobbuquera@gmail.com>
2024-06-03 16:20:40 -0300

    lista senac

---
+++ C#/app/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+// 1. Maior e menor em 10 inputs
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Entre com dez números inteiros:");
+
+        int maior = 0;
+        int menor = 10000000;
+
+        for (int i = 0; i < 10; i++)
+        {
+            int input = int.Parse(Console.ReadLine());
+            if (input > maior) maior = input;
+            if (input < menor) menor = input;
+        }
+
+        Console.WriteLine($"Maior: {maior}, menor: {menor}");
+    }
+}
