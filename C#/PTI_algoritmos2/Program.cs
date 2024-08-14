0ecb809105c60bfe5b9701a933f601f153f2a1ba
Rodrigo-Brezolin-Buquera <rodrigobbuquera@gmail.com>
2024-08-14 16:27:29 -0300

    PTI - Algortimo2 - Menu de opções

---
+++ C#/PTI_algoritmos2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace PTI_algoritmos2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bem vindo ao nosso sistema de estoque, escolha uma das opções abaixo");
+        showMenu();
+    }
+
+    static int showMenu()
+    {
+        bool validOption = false;
+        int option = -1;
+
+        while (validOption == false)
+        {
+            Console.WriteLine(" [1] Novo \n [2] Listar Produtos\n [3] Remover Produtos \n [4] Entrada Estoque\n [5] Saída Estoque\n [0] Sair");
+            option = int.Parse(Console.ReadLine());
+
+            if (option >= 0 && option < 6)
+            {
+                validOption = true;
+            }
+            else
+            {
+                Console.WriteLine("Entre com um opção válida");
+
+            }
+        }
+
+        return option;
+    }
+}
