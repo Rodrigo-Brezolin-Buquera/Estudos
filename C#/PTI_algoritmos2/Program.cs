1fd816b7a481011b989940dd9fce1056537b7ad5
Rodrigo-Brezolin-Buquera <rodrigobbuquera@gmail.com>
2024-08-14 16:41:31 -0300

    PTI - Algo2: esqueleto funções + TDA Livros

--- C#/PTI_algoritmos2/Program.cs
+++ C#/PTI_algoritmos2/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Bem vindo ao nosso sistema de estoque, escolha uma das opções abaixo");
-        showMenu();
+        int option = showMenu();
     }
 
     static int showMenu()
@@ -25,10 +25,46 @@ class Program
             else
             {
                 Console.WriteLine("Entre com um opção válida");
-
             }
         }
 
         return option;
+    }
+
+    static void addProduct() { }
+
+    static void showProducts() { }
+
+    static void removeProduct() { }
+
+    static void changeQuantity() { }
+
+    public class Book
+    {
+        string name;
+        string author;
+        double price;
+        string genre;      
+        int quantity;
+
+        public Book(string n, string a, double p, string g)
+        {
+            name = n;
+            author = a;
+            price = p;
+            genre = g;
+            quantity = 0;
+        }
+
+        public void addQuantity(int value)
+        {
+
+        }
+
+        public void removeQuantity(int value)
+        {
+
+        }
+
     }
 }
